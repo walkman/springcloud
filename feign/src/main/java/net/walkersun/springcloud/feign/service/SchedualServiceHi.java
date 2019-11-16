bdd8958d1042c13239dadb305936453a0ab62679
sunjin <sunjin9418@163.com>
2019-11-16 16:48:24 +0800

    hystrix

--- feign/src/main/java/net/walkersun/springcloud/feign/service/SchedualServiceHi.java
+++ feign/src/main/java/net/walkersun/springcloud/feign/service/SchedualServiceHi.java
@@ -1,13 +1,15 @@
 package net.walkersun.springcloud.feign.service;
 
+import net.walkersun.springcloud.feign.service.impl.SchedualServiceHiHystric;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
 }
+
 
---
+++ feign/src/main/java/net/walkersun/springcloud/feign/service/impl/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package net.walkersun.springcloud.feign.service.impl;
+
+import net.walkersun.springcloud.feign.service.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
