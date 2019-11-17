5fb685508d042db2ce29b6f4c06ab53befba6c8f
sunjin <sunjin9418@163.com>
2019-11-17 19:49:53 +0800

    cloud bus

--- configclient/src/main/java/net/walkersun/springcloud/configclient/ConfigClientApplication.java
+++ configclient/src/main/java/net/walkersun/springcloud/configclient/ConfigClientApplication.java
@@ -3,13 +3,22 @@ package net.walkersun.springcloud.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
+    /**
+     * POST: http://localhost:8881/actuator/bus-refresh
+     */
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
